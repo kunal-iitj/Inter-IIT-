a90f4b4f5e840153b5046e10793f7109c82be194
Kunal Tiwari <tiwari.25@iitj.ac.in>
2022-12-10 00:14:57 +0530

    added models for favorites

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'userInfo', 
+    'user',
     'rest_framework'
 ]
 
--- backend/userInfo/models.py
+++ backend/userInfo/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from user.models import User
 
 # Create your models here.
 
@@ -12,4 +13,8 @@ class Song(models.Model):
 
 
 class Favorite(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     song = Song()
+
+    def __str__(self):
+        return self.song.name
