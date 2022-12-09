efbcc30b310bb9fa0144e356ac9de8840bc939d2
Kunal Tiwari <tiwari.25@iitj.ac.in>
2022-12-10 00:11:01 +0530

    initialized the user app

--- backend/userInfo/models.py
+++ backend/userInfo/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class Song(models.Model):
+    name = models.CharField(max_length=100)
+    artist = models.CharField(max_length=100)
+    url = models.URLField()
+
+    def __str__(self):
+        return self.name
+
+
+class Favorite(models.Model):
+    song = Song()
