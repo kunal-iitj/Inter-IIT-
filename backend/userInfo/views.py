3b5b588cecacd138d6737e66801e150c43c93c86
Kunal Tiwari <tiwari.25@iitj.ac.in>
2022-12-10 21:37:47 +0530

    made functions to fetch token and data from the spotify api

--- backend/userInfo/views.py
+++ backend/userInfo/views.py
@@ -3,6 +3,7 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 import requests
 import base64
+from urllib.parse import urlencode
 
 # Create your views here.
 
@@ -14,11 +15,11 @@ def getAccessToken():
     client_b64 = base64.b64encode(client_credentials.encode())
 
     token_data = {
-        'grant-type': 'client_credntials'
+        'grant_type': 'client_credentials'
     }
 
     token_headers = {
-        'Authorization': f'{client_b64.decode()}'
+        'Authorization': f'Basic {client_b64.decode()}'
     }
 
     req = requests.post(token_url, data=token_data, headers=token_headers)
@@ -26,7 +27,25 @@ def getAccessToken():
     token = respone_data['access_token']
     return token
 
+def fetchAlbum(token):
+    headers = {
+        'Authorization': f'Bearer {token}'
+    }
+    endpoint = 'https://api.spotify.com/v1/search'
+    data = urlencode(
+        {
+            'q': 'By Your Side',
+            'type': 'track'
+        }
+    )
+
+    lookup_url = f'{endpoint}?{data}'
+    req = requests.get(lookup_url, headers=headers)
+    response = req.json()
+    return response
+
 @api_view(['GET'])
 def fetchData(request):
     token = getAccessToken()
-    return Response({'token': token})
+    response = fetchAlbum(token)
+    return Response(response)
