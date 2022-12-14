137616163474a18c58bff21b41ee2b6038c719a5
Aakash <aakash78.win@gmail.com>
2022-12-14 23:31:19 +0530

    added API calls to Login page

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,6 +1,40 @@
 import React from "react";
+import { useState } from "react";
+import { useLoginUserMutation } from "../services/userAuthApi";
+import { storeToken, removeToken } from "../services/localStorage";
 
-export default function LogIN(){
+export default function Login(){
+
+    const [serverError, setServerError] = useState({})
+    const [loginUser, {isLoading}] = useLoginUserMutation()
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        const data = new FormData(e.currentTarget);
+        const actualData = {
+          email: data.get('email'),
+          password: data.get('password'),
+        }
+        const res = await loginUser(actualData)
+        if (res.error) {
+            console.log(typeof (res.error.data.errors))
+            console.log(res.error)
+            setServerError(res.error.data.errors)
+
+            for(var key in serverError){
+            var value = serverError[key]
+            alert([key + ": " + value[0]])
+            }
+            removeToken()
+
+        }
+        if (res.data) {
+          console.log(res.data.token)
+          storeToken(res.data.token)
+        }
+      }
+
+    
     return (
         <main className="login-page">
             <section className="login-left">
@@ -9,9 +43,9 @@ export default function LogIN(){
                         <h1>LogIn</h1>
                         <h6>Get back to jamming to your favorite tunes</h6>
                     </div>
-                    <form action="">
-                        <input type="text" placeholder="Username"/>
-                        <input type="password"  placeholder="Password"/>
+                    <form action="" id="login-form" onSubmit={handleSubmit}>
+                        <input type="email" placeholder="Email" name="email"/>
+                        <input type="password"  placeholder="Password" name="password"/>
                         <button type="submit">Login</button>
                     </form>
                     <div className="registration-link">
