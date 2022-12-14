b83228afcddeac7c824e3e26bbef2069eb7b06a9
Aakash <aakash78.win@gmail.com>
2022-12-14 23:28:29 +0530

    added a services folder with files for authAPI

---
+++ frontend/src/services/userAuthApi.js
@@ -0,0 +1,46 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+
+// Define a service using a base URL and expected endpoints
+export const userAuthApi = createApi({
+  reducerPath: 'userAuthApi',
+  baseQuery: fetchBaseQuery({ baseUrl: 'http://127.0.0.1:8000/auth/' }),
+  endpoints: (builder) => ({
+    registerUser: builder.mutation({
+      query: (user) => {
+        return {
+          url: 'register/',
+          method: 'POST',
+          body: user,
+          headers: {
+            'Content-type': 'application/json',
+          }
+        }
+      }
+    }),
+    loginUser: builder.mutation({
+      query: (user) => {
+        return {
+          url: 'login/',
+          method: 'POST',
+          body: user,
+          headers: {
+            'Content-type': 'application/json',
+          }
+        }
+      }
+    }),
+    getLoggedUser: builder.query({
+      query: (access_token) => {
+        return {
+          url: 'profile/',
+          method: 'GET',
+          headers: {
+            'authorization': `Bearer ${access_token}`,
+          }
+        }
+      }
+    }),
+  }),
+})
+
+export const { useRegisterUserMutation, useLoginUserMutation, useGetLoggedUserQuery } = userAuthApi
