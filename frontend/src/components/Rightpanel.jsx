8e71b2b38dbb0823d2aab811ea67bd78459c288c
Kunal Tiwari <tiwari.25@iitj.ac.in>
2022-12-16 13:47:06 +0530

    clean up code

--- frontend/src/components/Rightpanel.jsx
+++ frontend/src/components/Rightpanel.jsx
@@ -3,9 +3,13 @@ import search from './../Images/searchcolor.png'
 export default function Rightpanel() {
   return (
     <div>
-      <div className="search" ><label htmlFor="search"><h2>Quick Search</h2></label><div className="searchbar">
-          <input type="text" placeholder='Search' id="search"/><button><img src={search} alt="" /></button>
-        </div></div>
+      <div className="search" >
+        <label htmlFor="search"><h2>Quick Search</h2></label>
+        <div className="searchbar">
+          <input type="text" placeholder='Search' id="search" />
+          <button><img src={search} alt="" /></button>
+        </div>
+      </div>
       
     </div>
   )
