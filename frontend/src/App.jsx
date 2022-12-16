44b149c4e82a321573c92bd5f53aa4488c341cd1
Naman Goyal <naman280104@gmail.com>
2022-12-17 00:32:25 +0530

    playlist page added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,10 @@ import reactLogo from './assets/react.svg';
 import './App.css';
 import { BrowserRouter, Route,Routes,useRoutes} from "react-router-dom";
 import Home from './components/Home';
+import Playlist from './components/Playlist';
 import Login from './pages/Login'
+import Registration from './pages/Registration';
+
 function App() {
   
   return (
@@ -12,6 +15,8 @@ function App() {
     <Routes>
       <Route path='/' element={<Home/>}/>
       <Route path='/login' element={<Login/>}/>
+      <Route path='/signup' element={<Registration/>}/>
+      <Route path='/playlist' element={<Playlist/>}/>
     </Routes>
     </BrowserRouter>
     </>
