75725ffecf95e84951a89c86a47880376e6e3423
Kunal Tiwari <tiwari.25@iitj.ac.in>
2022-12-17 12:25:22 +0530

    made a view for the list of artists to be  found

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from . import views 
 urlpatterns = [
-    path('artist/', views.fetchArtist, name='artist'),
+    path('artist/', views.fetchRecommendedArtists, name='artist'),
     path('playlist/', views.fetchCategoryPlaylist, name='playlist'),
 
 ]
