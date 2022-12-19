31516411a0bfd755ac27023b7313cb667e3453a5
Niranjan <niranjanjangir38@gmail.com>
2022-12-19 14:26:53 +0530

    Added Questionnaire and it's components.

---
+++ frontend/src/components/Favoriteartistscard.jsx
@@ -0,0 +1,13 @@
+const Favoriteartistscard = () => {
+    return ( 
+        
+        <div className="scrollx"><img src="https://png.pngtree.com/png-vector/20190710/ourmid/pngtree-user-vector-avatar-png-image_1541962.jpg" alt="" /><div ><div className='artistname'>ArtistName</div>
+        <div className="artistfollower" id="followers">#Followers</div>    
+        </div></div>
+
+     );
+}
+ 
+export default Favoriteartistscard;
+
+
