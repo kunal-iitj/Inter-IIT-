e60276a00727cfb168a1ea34e8d55527dde9ee04
AM-78 <aakash78.win@gmail.com>
2022-12-21 01:08:03 +0530

    added end points for liked songs

--- backend/user/urls.py
+++ backend/user/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from user.views import UserRegistrationView, UserLoginView, UserProfileView
+from user.views import UserRegistrationView, UserLoginView, UserProfileView, AddLikedSongView, RemoveLikedSongView
 
 
 urlpatterns = [
     path('register/', UserRegistrationView.as_view(), name='register'),
     path('login/', UserLoginView.as_view(), name='login'),
     path('profile/', UserProfileView.as_view(), name='profile'),
+    path('addlikedsong/', AddLikedSongView.as_view(), name='addlikedsong'),
+    path('removelikedsong/', RemoveLikedSongView.as_view(), name='removelikedsong'),
 ]
