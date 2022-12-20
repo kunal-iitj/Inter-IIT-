c256a32fdd4681e98ab3c76acb95e5716752e0bf
AM-78 <aakash78.win@gmail.com>
2022-12-21 04:56:07 +0530

    completely new page for liked songs with new tiles

--- frontend/src/pages/LikePage.jsx
+++ frontend/src/pages/LikePage.jsx
@@ -1,66 +1,49 @@
-import React from 'react'
-import LikePhoto from '../Images/LikePhoto.jpg'
-import Leftpanel from '../components/Leftpanel';
-function LikePage() {
-  return (
-    <>
-    <div className="splithome leftbox">
-        <Leftpanel/>
-      </div>
-    <div className="splithome LikePage">
-        
-        <div className="edit_playlist">
-            <img id="likeProfile" src={LikePhoto} alt="Reload"/>
+import React from "react";
+import LikePhoto from "../Images/LikePhoto.jpg";
+import Leftpanel from "../components/Leftpanel";
+import Tile2 from "../components/Tile2";
+import { getLikedSongs } from "../services/localStorage";
 
-            <div className='Liked'>Liked Songs</div>
-            <div className="songCount">10 Songs</div>
-        </div>
+export default function LikePage() {
+    const likedSongs = getLikedSongs();
+    var likedSongsArray = [];
+    var a = 0;
+    var n = likedSongs.length;
 
+    for (let i = 0; i < n; i++) {
+        likedSongsArray.push({ Tile2: <Tile2 />, key: i });
+    }
 
+    return (
+        <>
+            <div className="splithome leftbox">
+                <Leftpanel />
+            </div>
+            <div className="splithome LikePage">
+                <div className="edit_playlist">
+                    <img id="likeProfile" src={LikePhoto} alt="Reload" />
 
-        <div className="container">
-            <div className="songList">
-                <div className="songItemContainer">
-                    <div className="songItem">
-                        <img alt="1"/>
-                        <span className="songName">#SongName</span>
-                        <span className="songlistplay"><span className="timestamp">05:34 <i id="0"
-                                    className="far songItemPlay fa-play-circle"></i> </span></span>
+                    <div className="Liked">Liked Songs</div>
+                    <div className="songCount">{n} Songs</div>
+                </div>
+                <div className="container">
+                    <div className="songList">
+                        <div className="songItemContainer">
+                            <div className="row">
+                                {likedSongsArray.map((i) => {
+                                    return (
+                                        <Tile2
+                                            songName={likedSongs[a]}
+                                            number={a}
+                                            key={a++}
+                                        />
+                                    );
+                                })}
+                            </div>
+                        </div>
                     </div>
-                    <div className="songItem">
-                        <img alt="1"/>
-                        <span className="songName">#SongName</span>
-                        <span className="songlistplay"><span className="timestamp">05:34 <i id="1"
-                                    className="far songItemPlay fa-play-circle"></i> </span></span>
-                    </div>
-                    <div className="songItem">
-                        <img alt="1"/>
-                        <span className="songName">#SongName</span>
-                        <span className="songlistplay"><span className="timestamp">05:34 <i id="2"
-                                    className="far songItemPlay fa-play-circle"></i> </span></span>
-                    </div>
-                    <div className="songItem">
-                        <img alt="1"/>
-                        <span className="songName">#SongName</span>
-                        <span className="songlistplay"><span className="timestamp">05:34 <i id="3"
-                                    className="far songItemPlay fa-play-circle"></i> </span></span>
-                    </div>
-                    <div className="songItem">
-                        <img alt="1"/>
-                        <span className="songName">#SongName</span>
-                        <span className="songlistplay"><span className="timestamp">05:34 <i id="4"
-                                    className="far songItemPlay fa-play-circle"></i> </span></span>
-                    </div>
-                    
-
                 </div>
             </div>
-        </div>
-    </div>
-
-    
-    </>
-  );
+        </>
+    );
 }
-
-export default LikePage;
