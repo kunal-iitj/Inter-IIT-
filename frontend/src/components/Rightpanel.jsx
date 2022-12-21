fd6242f3c40882661c4a36e4792bd20f0644bc13
Kunal Tiwari <tiwari.25@iitj.ac.in>
2022-12-21 18:57:30 +0530

    finished the search feature

--- frontend/src/components/Rightpanel.jsx
+++ frontend/src/components/Rightpanel.jsx
@@ -1,15 +1,20 @@
 import React, {useState} from 'react'
 import search from './../Images/searchcolor.png'
+import SearchCard from './SearchCard';
+
 export default function Rightpanel() {
 
   const [search, setSearch] = useState('')
+  const [data, setData] = useState({})
   const handleChange = (event) =>{
     setSearch(event.target.value)
     console.log(search);
   }
 
   const handleSubmit = async ()=> {
-    const response = await fetch('')
+    const response = await fetch('http://127.0.0.1:8000/api/search', {method: 'GET', headers: {'Content-Type': 'application/json'}})
+    const data = await response.json()
+    setData(data)
   }
 
   return (
@@ -20,6 +25,7 @@ export default function Rightpanel() {
           <input type="text" placeholder='Search' id="search" value={search} onChange={handleChange}/>
           <button type='submit' onClick={handleSubmit}>Click</button>
         </div>
+        <SearchCard name={data.name} image={data.images} artist={data.artist}/>
       </div>
     </div>
   )
