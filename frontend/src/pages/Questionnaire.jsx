e39a0370620aa75823ccffb385cc2a5e71c8f1b7
Naman Goyal <naman280104@gmail.com>
2022-12-23 23:22:01 +0530

    onselected border added

--- frontend/src/pages/Questionnaire.jsx
+++ frontend/src/pages/Questionnaire.jsx
@@ -9,6 +9,7 @@ const Questionnaire = () => {
   const [languages, addLanguage] = useState([]);
 
   function handleClick(e){
+    document.getElementById(`${e.target.value}`).style.border =  "solid 4px red"
     addLanguage([...languages, e.target.value]);
     console.log(languages);
   }
@@ -53,22 +54,22 @@ const Questionnaire = () => {
       <img src={arrow} alt="" />
       </button>
       <div className="question1 question">
-        <button className="language" value='English'onClick={handleClick}>English</button>
-        <button className="language" value='Hindi' onClick={handleClick} >Hindi</button>
-        <button className="language" value='Filipino'onClick={handleClick}>Filipino</button>
-        <button className="language" value='Italian'onClick={handleClick}>Italian</button>
-        <button className="language" value='Spanish'onClick={handleClick}>Spanish</button>
-        <button className="language" value='Estonian'onClick={handleClick}>Estonian</button>
-        <button className="language" value='French' onClick={handleClick}>French</button>
-        <button className="language" value='Dutch' onClick={handleClick}>Dutch</button>
-        <button className="language" value='Portuguese' onClick={handleClick}>Portuguese</button>
-        <button className="language" value='Indonesian' onClick={handleClick}>Indonesian</button>
-        <button className="language" value='German' onClick={handleClick}>German</button>
-        <button className="language" value='Norwegian' onClick={handleClick}>Norwegian</button>
-        <button className="language" value='Danish' onClick={handleClick}>Danish</button>
-        <button className="language" value='Korean' onClick={handleClick}>Korean</button>
-        <button className="language" value='Romanian' onClick={handleClick}>Romanian</button>
-        <button className="language" value='Arabic' onClick={handleClick}>Arabic</button>
+        <button className="language" value='English' id="English"onClick={handleClick}>English</button>
+        <button className="language" value='Hindi' id="Hindi" onClick={handleClick} >Hindi</button>
+        <button className="language" value='Filipino'   id='Filipino'   onClick={handleClick}>Filipino</button>
+        <button className="language" value='Italian'    id='Italian'    onClick={handleClick}>Italian</button>
+        <button className="language" value='Spanish'    id='Spanish'    onClick={handleClick}>Spanish</button>
+        <button className="language" value='Estonian'   id='Estonian'   onClick={handleClick}>Estonian</button>
+        <button className="language" value='French'     id='French'     onClick={handleClick}>French</button>
+        <button className="language" value='Dutch'      id='Dutch'      onClick={handleClick}>Dutch</button>
+        <button className="language" value='Portuguese' id='Portuguese' onClick={handleClick}>Portuguese</button>
+        <button className="language" value='Indonesian' id='Indonesian' onClick={handleClick}>Indonesian</button>
+        <button className="language" value='German'     id='German'     onClick={handleClick}>German</button>
+        <button className="language" value='Norwegian'  id='Norwegian'  onClick={handleClick}>Norwegian</button>
+        <button className="language" value='Danish'     id='Danish'     onClick={handleClick}>Danish</button>
+        <button className="language" value='Korean'     id='Korean'     onClick={handleClick}>Korean</button>
+        <button className="language" value='Romanian'   id='Romanian'   onClick={handleClick}>Romanian</button>
+        <button className="language" value='Arabic'     id='Arabic'     onClick={handleClick}>Arabic</button>
       </div>
         <button className="Generic-right" onClick={()=>rightScroll("question1")}>
           <img src={arrow} alt="" /></button>
